68cb6884707234a14c6d4f2ba19e9303014ede87
ScriptFizz <nsimamail@gmail.com>
2026-02-07 20:20:03 +0100

    extract skills added

--- src/job_plat/cli.py
+++ src/job_plat/cli.py
@@ -1,17 +1,30 @@
 import typer
 from datetime import date
 from job_plat.ingestion.run_scrape import run_indeed_scrape
+from job_plat.nlp.run_extract_skills import run_extract_skills
 
 app = typer.Typer()
 
 @app.command()
-def scrape_indeed(
+def scrape_indeed_cli(
     run_date: date = typer.Option(),
     query: str = "data engineer",
     location: str = "Milan"
-):
+) -> None:
     count = run_indeed_scrape(run_date = run_date, query = query, location = location)
     typer.echo(f"Scrapped {count} jobs from Indeed")
+
+
+@app.command()
+def extract_skills_cli(
+    silver_path: str | None = None,
+    gold_path: str | None = None
+) -> None:
+
+    run_extract_skills(
+        silver_path=silver_path,
+        gold_path=gold_path
+    )
 
 if __name__ == "__main__":
     app()
---
+++ src/job_plat/nlp/skills.py
@@ -0,0 +1,12 @@
+
+RAW_SKILLS = [
+    "python", "pyspark", "spark", "sql", "aws", "gcp",
+    "bigquery", "airflow", "docker", "kubernetes",
+    "nlp", "spacy", "pandas", "tensorflow", "mlflow"
+]
+
+SKILL_SYNONYMS = {
+    "py spark": "pyspark",
+    "google cloud": "gcp",
+    "amazon web services": "aws",
+}
