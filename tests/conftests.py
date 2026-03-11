046bf7e219e3dce2868058d028124a8849b9f71c
ScriptFizz <nsimamail@gmail.com>
2026-03-12 00:29:22 +0100

    testing fix

---
+++ tests/conftests.py
@@ -0,0 +1,13 @@
+import pytest
+from pyspark.sql import SparkSession
+
+@pytest.fixture(scope="session")
+def spark():
+    spark = (
+        SparkSession.builder
+        .master("local[*]")
+        .appName("test")
+        .getOrCreate()
+    )
+    yield spark
+    spark.stop()
--- tests/test_data.py
+++
@@ -1,5 +0,0 @@
-import pytest
-
-
-def test_code_is_tested():
-    assert 1 + 1 == 2
